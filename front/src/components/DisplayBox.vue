121842bb0b9f631d2bc208027a93cab20eaccc03
Amondt <mondt.alexande>
2019-04-15 21:26:26 +0200

    display beers style

--- front/src/components/DisplayBox.vue
+++ front/src/components/DisplayBox.vue
@@ -1,12 +1,14 @@
 <template>
     <div>
         <v-container>
-            <v-layout wrap justify-space-between v-if="displayBeers && displayBeers.length">
-                <v-flex v-for="beer in displayBeers" :key="beer.name" mx-3 my-3 xs2>
-                    <p>{{ beer.name }}</p>
-                    <p>ABV : {{ beer.abv }}</p>
-                    <p>IBU : {{ beer.ibu }}</p>
-                    <p>EBC : {{ beer.ebc }}</p>
+            <v-layout wrap justify-space-around v-if="displayBeers && displayBeers.length">
+                <v-flex v-for="beer in displayBeers" :key="beer.name" mx-1 my-3 xs3>
+                    <v-card flat hover style="height:100%;">
+                        <v-card-title>{{ beer.name }}</v-card-title>
+                        <v-card-text>{{ beer.abv }} %</v-card-text>
+                        <img :src="beer.image_url ? beer.image_url : ''">
+                        <v-card-text>{{ beer.tagline }}</v-card-text>
+                    </v-card>
                 </v-flex>
             </v-layout>
             <v-layout v-else>
@@ -28,5 +30,7 @@ export default {
 </script>
 
 <style scoped>
-
+img {
+    height: 200px;
+}
 </style>
