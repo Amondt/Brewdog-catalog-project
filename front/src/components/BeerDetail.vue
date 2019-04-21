00b5a538d64ea88abf3f11db00f6b77dda732473
Amondt <mondt.alexande>
2019-04-21 11:35:43 +0200

    style dialog

--- front/src/components/BeerDetail.vue
+++ front/src/components/BeerDetail.vue
@@ -8,44 +8,46 @@
         </v-card>
         <v-dialog v-model="detailDisplay" max-width="500">
             <v-card>
-                <v-card-title>{{ beer.name }}</v-card-title>
+                <v-card-title class="title grey darken-2 grey--text text--lighten-2 text-xs-center">{{ beer.name }}</v-card-title>
 
-                <v-card-text>{{ beer.tagline }}</v-card-text>
+                <v-card-text class="grey--text text--lighten-2 subheading text-xs-left">{{ beer.tagline }}</v-card-text>
 
-                <v-layout>
-                    <v-flex xs4>
+                <v-divider></v-divider>
+
+                <v-layout mt-3>
+                    <v-flex xs4 align-self-center>
                         <img height="150" :src="beer.image_url" :alt="beer.name + ' | ' + beer.image_url">
                     </v-flex>
 
-                    <v-flex xs6>
-                        <v-card-text>{{ beer.description }}</v-card-text>
+                    <v-flex xs8 px-3>
+                        <v-card-text text-xs-left class="grey--text text--lighten-2 body-1 text-xs-left">{{ beer.description }}</v-card-text>
                     </v-flex>
                 </v-layout>
 
                 <v-layout justify-space-between>
                     <v-flex>
-                        <v-chip>ABV: {{ beer.abv }} %</v-chip>
+                        <v-chip disabled class="grey--text text--lighten-2">ABV: {{ beer.abv }} %</v-chip>
                     </v-flex>
 
                     <v-flex>
-                        <v-chip>IBU: {{ beer.ibu }}</v-chip>
+                        <v-chip disabled class="grey--text text--lighten-2">IBU: {{ beer.ibu }}</v-chip>
                     </v-flex>
 
                     <v-flex>
-                        <v-chip>EBC: {{ beer.ebc }}</v-chip>
+                        <v-chip disabled class="grey--text text--lighten-2">EBC: {{ beer.ebc }}</v-chip>
                     </v-flex>
                 </v-layout>
 
-                <v-layout>
+                <v-layout mt-3>
                     <v-flex xs6>
                         <v-expansion-panel>
                             <v-expansion-panel-content>
                                 <template v-slot:header>
-                                    <div>malts</div>
+                                    <div class="grey--text text--lighten-2">malts</div>
                                 </template>
-                                <v-card>
-                                    <v-card-text v-for="(malt, index) in beer.ingredients.malt" :key="index">
-                                        {{ malt.name }}
+                                <v-card flat>
+                                    <v-card-text v-for="(malt, index) in malts" :key="index" class="grey--text text--lighten-2">
+                                        {{ malt }}
                                     </v-card-text>
                                 </v-card>
                             </v-expansion-panel-content>
@@ -56,11 +58,11 @@
                         <v-expansion-panel>
                             <v-expansion-panel-content>
                                 <template v-slot:header>
-                                    <div>hops</div>
+                                    <div class="grey--text text--lighten-2">hops</div>
                                 </template>
                                 <v-card>
-                                    <v-card-text v-for="(hops, index) in beer.ingredients.hops" :key="index">
-                                        {{ hops.name }}
+                                    <v-card-text v-for="(hop, index) in hops" :key="index" class="grey--text text--lighten-2">
+                                        {{ hop }}
                                     </v-card-text>
                                 </v-card>
                             </v-expansion-panel-content>
@@ -68,9 +70,9 @@
                     </v-flex>
                 </v-layout>
 
-                <v-card-text>yeast: {{ beer.ingredients.yeast }}</v-card-text>
+                <v-card-text class="grey--text text--lighten-2">yeast: {{ beer.ingredients.yeast }}</v-card-text>
 
-                <v-card-text>tips: {{ beer.brewers_tips }}</v-card-text>
+                <v-card-text class="grey--text text--lighten-2">tips: {{ beer.brewers_tips }}</v-card-text>
             </v-card>
         </v-dialog>
     </div>
@@ -88,6 +90,26 @@ export default {
         return {
             detailDisplay: false
         }
+    },
+    computed: {
+        hops: function() {
+            let hops = []
+            this.beer.ingredients.hops.forEach(hop => {
+                if (!hops.includes(hop.name)) {
+                    hops.push(hop.name)
+                }
+            })
+            return hops
+        },
+        malts: function() {
+            let malts = []
+            this.beer.ingredients.malt.forEach(malt => {
+                if (!malts.includes(malt.name)) {
+                    malts.push(malt.name)
+                }
+            })
+            return malts
+        }
     }
 }
 </script>
@@ -98,5 +120,21 @@ export default {
     z-index: 1;
     top: 65px;
     right: 2px;
+}
+
+::-webkit-scrollbar {
+  width: 10px;
+}
+
+::-webkit-scrollbar-track {
+  background: #424242;
+}
+
+::-webkit-scrollbar-thumb {
+  background: #888; 
+}
+
+::-webkit-scrollbar-thumb:hover {
+  background: #555; 
 }
 </style>
